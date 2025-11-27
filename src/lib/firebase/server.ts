4e55dab18e271c0549a37da38e3d82071c3a66a9
Mike Friedman <mike@comorgsolutions.info>
2025-11-27 15:00:57 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -1,4 +1,5 @@
 
+import "dotenv/config";
 import { initializeApp, getApps, App, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';

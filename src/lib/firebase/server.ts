4e6a4e4e0fcf2e2df425bcf122cbf697d8e99506
Mike Friedman <mike@comorgsolutions.info>
2025-11-27 21:46:43 +0000

    /fixError '
    1/1
    
    Next.js 15.3.3 (outdated)
    Turbopack
    Runtime Error
    Serv

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -20,27 +20,10 @@ function getAdminApp(): { app: App; auth: Auth; db: Firestore } {
   if (existingApp) {
     adminApp = existingApp;
   } else {
-    // This is the part that has been failing.
-    const serviceAccount = {
-      "type": "service_account",
-      "project_id": "studio-9020847636-9d4fa",
-      "private_key_id": "e4f8d298538b30f81d137b349b109e543666d66e",
-      "privateKey": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCp\n-----END PRIVATE KEY-----\n".replace(/\\n/g, '\n'),
-      "client_email": "firebase-adminsdk-3y52g@studio-9020847636-9d4fa.iam.gserviceaccount.com",
-      "client_id": "111360058319690184423",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-3y52g%40studio-9020847636-9d4fa.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
-    };
-
-    adminApp = initializeApp(
-      {
-        credential: cert(serviceAccount),
-      },
-      'admin'
-    );
+    // Use Application Default Credentials.
+    // This is the correct way to initialize in a managed Google Cloud environment.
+    // The SDK will automatically find the service account credentials.
+    adminApp = initializeApp(undefined, 'admin');
   }
 
   adminAuth = getAuth(adminApp);

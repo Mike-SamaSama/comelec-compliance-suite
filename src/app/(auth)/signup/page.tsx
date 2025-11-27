263e11285aed55f3f0a18e9df8aa1ad3c7cb2f0b
Mike Friedman <mike@comorgsolutions.info>
2025-11-27 13:00:45 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useActionState, useFormStatus } from "react";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
